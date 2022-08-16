bc3a8d8855e4f2d1d0cca86973dc75f092f8d785
Rebecca MacPhee <macphee.rebecca@gmail.com>
2022-08-16 17:07:28 -0400

    app skeleton

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1>My react app is live!</h1>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+express()
+
+    .get('/', (req, res) => {
+        res.status(200).json({status: 200, message: "Hello!"})
+    })
+
+    .listen(PORT, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+    
+;
